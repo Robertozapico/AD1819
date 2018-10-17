e232ca38ef06d30ca7d4aa91ca68e6611f275ce8
Robertozapico <robertozapicoblanco@gmail.com>
2018-10-17 23:07:40 +0200

    Comenzado ejercicio de entrega, añadida la función de escanear y de mirar los tamaños.

---
+++ aplicacionAD/src/Logica/LogicaMetodos.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import java.io.File;
+
+/**
+ *
+ * @author zapia
+ */
+public class LogicaMetodos {
+
+    
+    /**
+     * 
+     * @return 
+     */
+    public File[] obtenerParticiones() {
+        File[] particiones = File.listRoots();
+        return particiones;
+    }
+
+    /**
+     *
+     * @param unidad
+     * @return
+     */
+    public int espacioLibreDisponible(String unidad) {
+        File unidadSeleccionada = new File(unidad);
+
+        int espacioDisponibleDeLaUnidad = (int) (unidadSeleccionada.getFreeSpace() / 1073741824);
+        return espacioDisponibleDeLaUnidad;
+    }
+/**
+ * 
+ * @param rutaEscaneada
+ * @return 
+ */
+    public File[] escanearUnidad(String rutaEscaneada) {
+        File unidadSeleccionada = new File(rutaEscaneada);
+        File[] ficherosDeLaUnidad = unidadSeleccionada.listFiles();
+        int ficheroCogido;
+        for (ficheroCogido = 0; ficheroCogido < ficherosDeLaUnidad.length; ficheroCogido++) {
+            if (ficherosDeLaUnidad[ficheroCogido].isDirectory()) {
+
+                escanearUnidad(ficherosDeLaUnidad[ficheroCogido].getAbsolutePath());
+            }
+            System.out.println(ficherosDeLaUnidad[ficheroCogido].getName());
+        }
+        System.out.println("-------------------------------------------");
+        return ficherosDeLaUnidad;
+    }
+
+    /*
+    Sugiere  eliminar  directoriosvacíos.
+    •Sugiere  eliminar  ficherosde  unas  determinadas  categorías.
+    •Sugiere  eliminar  ficheros  de  gran  tamaño.
+    •Sugiere  eliminar  ficheros  antiguos.
+    •Busca  ficheros  duplicados
+    */
+}
--- aplicacionAD/src/Logica/PantallaIniciar.java
+++ aplicacionAD/src/Logica/PantallaIniciar.java
@@ -1,20 +1,20 @@
-package Logica;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Logica;
+
 /**
  *
- * @author alumnop
+ * @author zapia
  */
-public class PantallaInicial extends javax.swing.JFrame {
+public class PantallaIniciar extends javax.swing.JFrame {
 
     /**
-     * Creates new form PantallaInicial
+     * Creates new form PantallaIniciar
      */
-    public PantallaInicial() {
+    public PantallaIniciar() {
         initComponents();
     }
 
@@ -29,19 +29,19 @@ public class PantallaInicial extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jButtonEscanear = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 204, 153));
 
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        jLabel1.setText("Bienvenido a la aplicación de limpieza de disco");
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setText("Bienvenido a la limpieza de disco duro");
 
-        jButtonEscanear.setText("Escanear unidad");
-        jButtonEscanear.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("Escanear");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonEscanearActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
 
@@ -49,43 +49,41 @@ public class PantallaInicial extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 527, Short.MAX_VALUE)
-                .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(144, 144, 144)
-                .addComponent(jButtonEscanear, javax.swing.GroupLayout.PREFERRED_SIZE, 224, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(24, 24, 24)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(51, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addComponent(jLabel1)
-                .addGap(99, 99, 99)
-                .addComponent(jButtonEscanear)
-                .addContainerGap(128, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(93, 93, 93)
+                .addComponent(jButton1)
+                .addContainerGap(136, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButtonEscanearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonEscanearActionPerformed
-        PantallaUnidades pantallaUnidades = new PantallaUnidades(this, true);
-        pantallaUnidades.setVisible(true);        
-    }//GEN-LAST:event_jButtonEscanearActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        PantallaEscaneo pantallaEscaneo = new PantallaEscaneo(this,true);
+        pantallaEscaneo.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -104,26 +102,26 @@ public class PantallaInicial extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PantallaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PantallaIniciar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PantallaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PantallaIniciar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PantallaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PantallaIniciar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PantallaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PantallaIniciar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PantallaInicial().setVisible(true);
+                new PantallaIniciar().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonEscanear;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
