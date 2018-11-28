c75f99cb61d54a84c07e3f6bd8c68b7ef7477f8a
RobertoZapico <robertozapicoblanco@gmail.com>
2018-11-28 14:21:41 +0100

    Comenzado ejercicio 3

---
+++ Ejercicio3Jason/src/ejercicio3jason/Metodos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio3jason;
+
+/**
+ *
+ * @author alumnop
+ */
+public class Metodos {
+    //Crea  un  fichero  JSON  (jsonWriter)  con  la  estructura  del  XSD  que  añade  un  cliente  con  dos  direcciones.
+
+    public void crearFicheroJSON() {
+        
+    }
+
+}
