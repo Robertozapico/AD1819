a0458ef0df5ca94fb80203e15725913130865199
RobertoZapico <robertozapicoblanco@gmail.com>
2018-11-30 12:20:02 +0100

    CreacionCliente hecha, falta añadir direccion

--- Ejercicio3Jason/src/ejercicio3jason/Ejercicio3Jason.java
+++ Ejercicio3Jason/src/ejercicio3jason/Ejercicio3Jason.java
@@ -5,6 +5,12 @@
  */
 package ejercicio3jason;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonObject;
+
 /**
  *
  * @author alumnop
@@ -15,7 +21,68 @@ public class Ejercicio3Jason {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Metodos metodos = new Metodos();
         
+        /*JsonObject cliente1 = Json.createObjectBuilder()
+                .add("apellido", "Sanz")
+                .add("apellido", "Sanchez")
+                .add("direccion", Json.createArrayBuilder()//creamos el corchete
+                        .add(Json.createObjectBuilder()
+                                .add("calle", "callecita")
+                                .add("numero", 53)
+                                .add("piso", 2)
+                                .add("escalera", "c")
+                                .add("cp", 33940)
+                                .add("ciudad", "El Entrego")
+                        )//},  
+                        .add(Json.createObjectBuilder()
+                                .add("calle", "callina")
+                                .add("numero", 13)
+                                .add("piso", 6)
+                                .add("escalera", "A")
+                                .add("cp", 33940)
+                                .add("ciudad", "El Entrego")
+                        )//},  
+                )//],
+                .add("telefono", 342123242)
+                .add("nombre", "Paco")
+                .build();//para cerrarlo:  }]
+
+        JsonObject cliente2 = Json.createObjectBuilder()
+                .add("apellido", "Galvez")
+                .add("apellido", "Amaral")
+                .add("direccion", Json.createArrayBuilder()//creamos el corchete
+                        .add(Json.createObjectBuilder()
+                                .add("calle", "titanic")
+                                .add("numero", 52)
+                                .add("piso", 2)
+                                .add("escalera", "c")
+                                .add("cp", 33940)
+                                .add("ciudad", "El Entrego")
+                        )//},  
+                        .add(Json.createObjectBuilder()
+                                .add("calle", "callina")
+                                .add("numero", 85)
+                                .add("piso", 6)
+                                .add("escalera", "A")
+                                .add("cp", 33940)
+                                .add("ciudad", "El Entrego")
+                        )//},  
+                )//],
+                .add("telefono", 342123242)
+                .add("nombre", "Juancar")
+                .build();//para cerrarlo:  }]
+        
+        
+        try {
+            metodos.crearFicheroJSON("ficheroJSON.json", cliente1, cliente2);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio3Jason.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        */
+        
+        JsonObject cliente=metodos.crearClienteJson("Paco", "Pacoape", "callecita", 32, 2, 'C', 33940, "El Entrego", 89345345, "nombre");
+        System.out.println(metodos.annadirDireccion(cliente, "prueba", 0, 0, 'A', 0, "Laviana"));
     }
     
 }
--- Ejercicio3Jason/src/ejercicio3jason/Metodos.java
+++ Ejercicio3Jason/src/ejercicio3jason/Metodos.java
@@ -5,15 +5,94 @@
  */
 package ejercicio3jason;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonWriter;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.transform.stream.StreamSource;
+
 /**
  *
  * @author alumnop
  */
 public class Metodos {
     //Crea  un  fichero  JSON  (jsonWriter)  con  la  estructura  del  XSD  que  añade  un  cliente  con  dos  direcciones.
+/*
+    private JAXBElement jaxbElement = null;
+    private javax.xml.bind.JAXBContext jaxbCtx = null;
+    private Marshaller m = null;
 
-    public void crearFicheroJSON() {
-        
+    public JAXBElement unmarshalizar(File documentoXML, String nombrePaqueteDeLasClasesGeneradasPorElXML) {
+        try {
+            jaxbCtx = javax.xml.bind.JAXBContext.newInstance(nombrePaqueteDeLasClasesGeneradasPorElXML);
+            javax.xml.bind.Unmarshaller unmarshaller = jaxbCtx.createUnmarshaller();
+            jaxbElement = (JAXBElement) unmarshaller.unmarshal(new StreamSource(new java.io.File(documentoXML.toString())), Clientes.class); //NOI18N
+            //Clientes clientes = (Clientes) new Clientes();
+            //clientes = (Clientes) jaxbElement.getValue();
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+        }
+        return jaxbElement;
+    }*/
+    public JsonObject crearClienteJson(String apellido1, String apellido2, String calle, int numero, int piso, char escalera, int cp, String ciudad, int telefono, String nombre) {
+        JsonObject cliente = Json.createObjectBuilder()
+                .add("apellido", apellido1)
+                .add("apellido", apellido2)
+                .add("direccion", Json.createArrayBuilder()//creamos el corchete
+                        .add(Json.createObjectBuilder()
+                                .add("calle", calle)
+                                .add("numero", numero)
+                                .add("piso", piso)
+                                .add("escalera", escalera)
+                                .add("cp", cp)
+                                .add("ciudad", ciudad)
+                        )//}, 
+                )//],
+                .add("telefono", telefono)
+                .add("nombre", nombre)
+                .build();//para cerrarlo:  }]
+        return cliente;
+    }
+
+    public JsonObject annadirDireccion(JsonObject cliente, String calle, int numero, int piso, char escalera, int cp, String ciudad) {
+
+        for (int n = 0; n < cliente.getJsonArray("direccion").size(); n++) {
+            JsonObject object = cliente.getJsonArray("direccion").getJsonArray(n).getJsonObject(n);
+            // do some stuff....
+        }
+        /*.add(Json.createObjectBuilder()
+                    .add("calle", calle)
+                    .add("numero", numero)
+                    .add("piso", piso)
+                    .add("escalera", escalera)
+                    .add("cp", cp)
+                    .add("ciudad", ciudad)
+        );*/
+        return cliente;
+    }
+
+    public void crearFicheroJSON(String rutaFichero, JsonObject cliente1, JsonObject cliente2) throws IOException {
+
+        JsonArray arrayJsonLibros = Json.createArrayBuilder().add(cliente1)
+                .add(cliente2)
+                .build();
+        FileWriter ficheroSalida = new FileWriter(rutaFichero);
+        JsonWriter jsonWriter = Json.createWriter(ficheroSalida);
+        jsonWriter.writeArray(arrayJsonLibros);
+        ficheroSalida.flush();
+        ficheroSalida.close();
+
+    }
+
+    public void contarTotalClientes() {
+
     }
 
 }
